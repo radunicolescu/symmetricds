dd1ab968ac903e4d1b08f1fbf36bc4142753e335
radunicolescu <nicolescu.radu@gmail.com>
2026-04-08 14:50:14 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-# Swap variables in the template and create the actual engine file
-# We use 'envsubst' which is a standard Linux utility
+# Inject variables into the template
 envsubst < /opt/symmetric-ds/engines/corp-000.properties.template > /opt/symmetric-ds/engines/corp-000.properties
 
-echo "Engine file generated. Starting SymmetricDS..."
+echo "Engine file generated successfully."
 
-# Start SymmetricDS (this is the original command the image uses)
+# Start SymmetricDS
 exec /opt/symmetric-ds/bin/sym
